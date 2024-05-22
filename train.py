62f1e280791147d1daa50f6fdd9594ebcfcc1ba8
pc131375 <p.ciulkowicz@gmail.com>
2024-05-22 22:29:28 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import numpy as np
-from sklearn.linear_model import Perceptron
+from sklearn.linear_model import cw2
 import pickle
 
 X = np.array([[0, 0], [1, 1], [1, 0], [0, 1]])
 y = np.array([0, 1, 1, 0])
 
-model = Perceptron()
+model = cw2()
 model.fit(X, y)
 
 with open('model.pkl', 'wb') as f:
